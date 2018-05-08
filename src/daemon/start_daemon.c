26af264233aa3c43be0bf2f853c93467ffd24dae
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2018-05-08 11:13:38 +0200

    Functioning daemon

--- src/daemon/start_daemon.c
+++ src/daemon/start_daemon.c
@@ -3,11 +3,13 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define PIDPATH "/tmp/xlog/pidpath.txt"
+#define PIDLENGTH 5
+#define PIDPATH "/tmp/xlog/pidpath"
 #define MAXLEN 80
 
 void daemonize() {
@@ -32,32 +34,29 @@ void daemonize() {
   // Secondo fork
   fid = frk();
 
-  // Termino il padre
-  if (fid <= 0) {
-    // exit(EXIT_SUCCESS);
-    // }
-
-    // Cambio directory
-    if (chdir("/") < 0) {
-      perror("Changing directory failure:");
-      exit(EXIT_FAILURE);
-    }
-
-    // Chiudo tutti i fd
-    for (int x = 0; x < sysconf(_SC_OPEN_MAX); x++) {
-      close(x);
-    }
-
-    // Setto pid corrente come il demone
-    int fd = open(PIDPATH, O_CREAT, 0755);
+  // Termino il padre e scrivo il pid
+  if (fid > 0) {
+    int fd = open(PIDPATH, O_WRONLY | O_CREAT, 0777);
     if (fd < 0) {
       perror("Error in creating file: ");
     }
 
-    char str[MAXLEN];
+    char str[PIDLENGTH + 1];
     sprintf(str, "%d", fid);
-    write(fd, str, MAXLEN);
+    write(fd, str, PIDLENGTH + 1);
     close(fd);
+    exit(EXIT_SUCCESS);
+  }
+
+  // Cambio directory
+  if (chdir("/") < 0) {
+    perror("Changing directory failure:");
+    exit(EXIT_FAILURE);
+  }
+
+  // Chiudo tutti i fd
+  for (int x = 0; x < sysconf(_SC_OPEN_MAX); x++) {
+    close(x);
   }
 }
 
