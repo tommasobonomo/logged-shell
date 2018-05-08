62aeaacf938710de1e25ebb6e3e6329b117fba15
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2018-05-08 17:37:22 +0200

    Initial executer commit

---
+++ src/executer/executer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Executes subcommand received from main file
+ * @param subcommand the subcommand that will be executed
+ */
+void executeSubCommand(char subcommand[])
+{
+}
---
+++ src/executer/executer.h
@@ -0,0 +1,10 @@
+#ifndef EXECUTER_H
+#define EXECUTER_H
+
+/**
+ * Executes subcommand received from main file
+ * @param subcommand the subcommand that will be executed
+ */
+void executeSubCommand(char subcommand[]);
+
+#endif
