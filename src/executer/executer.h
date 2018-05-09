a14970f1403156df4e963fce92590dc530567d1a
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2018-05-09 21:29:31 +0200

    Corrected merge errors

--- src/executer/executer.h
+++ src/executer/executer.h
@@ -1,10 +1,14 @@
 #ifndef EXECUTER_H
 #define EXECUTER_H
 
+#include <sys/types.h>
+
 /**
- * Executes subcommand received from main file
+ * Executes subcommand received from main file in a forked process
+ * The parent process waits for it's child to finish execution before returning the PID
  * @param subcommand the subcommand that will be executed
+ * @return the PID of the process that executed the subcommand
  */
-void executeSubCommand(char subcommand[]);
+pid_t executeSubCommand(char subcommand[]);
 
 #endif
