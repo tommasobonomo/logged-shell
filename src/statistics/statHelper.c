22fbe92e82aa6e0e81b9b58b7897a6cdc9c6ae92
Federico Favotto <federico.favotto@studenti.unitn.it>
2018-05-22 10:02:54 +0200

    prettier formatted output

--- src/statistics/statHelper.c
+++ src/statistics/statHelper.c
@@ -40,33 +40,39 @@ void printStatsC(FILE *fp, Command *cmd)
     time_t now = time(NULL);
     struct tm nowFormatted = *localtime(&now);
 
-    fprintf(fp, "######### STATS #########\n");
+    fprintf(fp, "############################## STATS ############################\n");
     fprintf(fp, "#\n");
-    fprintf(fp, "# Command: %s\n", cmd->command);
-    fprintf(fp, "# n° subcommand: %d\n", cmd->n_subCommands);
-    fprintf(fp, "# %d-%02d-%02d %02d:%02d:%02d\n", nowFormatted.tm_year + 1900, nowFormatted.tm_mon + 1,
+    fprintf(fp, "#      %d-%02d-%02d %02d:%02d:%02d\n", nowFormatted.tm_year + 1900, nowFormatted.tm_mon + 1,
             nowFormatted.tm_mday, nowFormatted.tm_hour, nowFormatted.tm_min, nowFormatted.tm_sec);
-    fprintf(fp, "#\n\n");
+    fprintf(fp, "#\n");
+    fprintf(fp, "#      whole command: %s\n", cmd->command);
+    fprintf(fp, "#      n° subcommand: %d\n", cmd->n_subCommands);
+    fprintf(fp, "#\n");
     int i;
     for (i = 0; i < cmd->n_subCommands; i++)
     {
         printStatsS(fp, &cmd->subCommandResults[i]);
     }
-    fprintf(fp, "\n");
+    fprintf(fp, "#################################################################\n\n\n");
 }
 
 void printStatsS(FILE *fp, SubCommandResult *subCommandResult)
 {
-    fprintf(fp, "Subcommand: %s\n", subCommandResult->subCommand);
-    fprintf(fp, "  Exit status: %d \n", subCommandResult->exitStatus);
-    fprintf(fp, "  Real elapsed time: %f seconds \n", subCommandResult->totTime);
-    fprintf(fp, "  PID: %d\n", subCommandResult->pid);
-    fprintf(fp, "  system CPU time used: %ld μs\n", subCommandResult->cputime);
-    fprintf(fp, "  maximum resident set size: %ld kB\n", subCommandResult->vmressize);
-    fprintf(fp, "  page reclaims (soft page faults): %ld\n", subCommandResult->softPageFaults);
-    fprintf(fp, "  page faults (hard page faults): %ld\n", subCommandResult->hardPageFaults);
-    fprintf(fp, "  swaps: %ld\n", subCommandResult->swaps);
-    fprintf(fp, "  signals received: %ld\n", subCommandResult->signals);
-    fprintf(fp, "  voluntary context switches: %ld\n", subCommandResult->voluntary_ctxt_switches);
-    fprintf(fp, "  involuntary context switches: %ld\n\n", subCommandResult->nonvoluntary_ctxt_switches);
+    fprintf(fp, "#    ========================================================\n");
+    fprintf(fp, "#\n");
+    fprintf(fp, "#                 Subcommand: %s\n", subCommandResult->subCommand);
+    fprintf(fp, "#\n");
+    fprintf(fp, "#                        PID:%14d\n", subCommandResult->pid);
+    fprintf(fp, "#                exit status:%14d\n", subCommandResult->exitStatus);
+    fprintf(fp, "#               elapsed time:%14f s\n", subCommandResult->totTime);
+    fprintf(fp, "#              CPU time used:%14ld μs\n", subCommandResult->cputime);
+    fprintf(fp, "#               max ram size:%14ld kB\n", subCommandResult->vmressize);
+    fprintf(fp, "#           soft page faults:%14ld\n", subCommandResult->softPageFaults);
+    fprintf(fp, "#           hard page faults:%14ld\n", subCommandResult->hardPageFaults);
+    fprintf(fp, "#                      swaps:%14ld\n", subCommandResult->swaps);
+    fprintf(fp, "#           signals received:%14ld\n", subCommandResult->signals);
+    fprintf(fp, "#           signals received:%14ld\n", subCommandResult->voluntary_ctxt_switches);
+    fprintf(fp, "#      vol. context switches:%14ld\n", subCommandResult->voluntary_ctxt_switches);
+    fprintf(fp, "#      inv. context switches:%14ld\n", subCommandResult->nonvoluntary_ctxt_switches);
+    fprintf(fp, "#\n");
 }
